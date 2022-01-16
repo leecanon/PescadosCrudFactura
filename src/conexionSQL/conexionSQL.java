d0a9b0c5edb7dc4f4109aeb0f51080c9afe0e528
leecanon <victor.chuquin@upeu.edu.pe>
2022-01-16 06:15:54 -0500

    conexion-sql

--- src/conexionSQL/conexionSQL.java
+++ src/conexionSQL/conexionSQL.java
@@ -5,10 +5,28 @@
  */
 package conexionSQL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Usuario
  */
 public class conexionSQL {
     
+    Connection conectar=null;
+    
+    public Connection conexion(){
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar=DriverManager.getConnection("jdbc:mysql://localhost/pescados","root","");
+            JOptionPane.showMessageDialog(null, "Conexion Exitosa");
+            
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error de conexion " + e.getMessage());
+        }
+        return conectar;
+    }
 }
